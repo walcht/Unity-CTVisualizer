0b39b154646c5373125006814353678afff6f912
walid chtioui <walid.chtioui.main@gmail.com>
2024-01-17 01:19:27 +0100

    improve performance; add single DICOM file support;

--- uvdsconverter/uvds.py
+++ uvdsconverter/uvds.py
@@ -6,8 +6,6 @@ import re
 import numpy as np
 from dataclasses import dataclass
 from numpy.typing import NDArray
-import struct
-import math
 import logging
 
 texture_format_mapping = {
@@ -19,14 +17,14 @@ texture_format_mapping = {
 
 @dataclass
 class UVDS:
-    imagewidth: int
-    imageheight: int
-    nbrslices: int
-    voxeldimX: float
-    voxeldimY: float
-    voxeldimZ: float
-    textureformat: int
-    densities: List[float]
+    imagewidth: np.uint16
+    imageheight: np.uint16
+    nbrslices: np.uint16
+    voxeldimX: np.float32
+    voxeldimY: np.float32
+    voxeldimZ: np.float32
+    textureformat: np.uint8
+    densities: NDArray[np.float32]
 
 
 def get_voxel_depth(sliceLocations: List[float]) -> float:
@@ -40,11 +38,14 @@ def write_udvs_binary(uvds_data: UVDS, uvds_fp: str) -> None:
     # properly serialize output according to UDVS format
     logging.info(f"writing binary UVDS data to: {uvds_fp} ...")
     with open(uvds_fp, "wb") as output:
-        output.write(struct.pack("HHH", uvds_data.imagewidth, uvds_data.imageheight, uvds_data.nbrslices))
-        output.write(struct.pack("fff", uvds_data.voxeldimX, uvds_data.voxeldimY, uvds_data.voxeldimZ))
-        output.write(struct.pack("H", uvds_data.textureformat))
-        for i in range(len(uvds_data.densities)):
-            output.write(struct.pack("f", uvds_data.densities[i]))
+        output.write(uvds_data.imagewidth.tobytes())
+        output.write(uvds_data.imageheight.tobytes())
+        output.write(uvds_data.nbrslices.tobytes())
+        output.write(uvds_data.voxeldimX.tobytes())
+        output.write(uvds_data.voxeldimY.tobytes())
+        output.write(uvds_data.voxeldimZ.tobytes())
+        output.write(uvds_data.textureformat.tobytes())
+        output.write(uvds_data.densities.tobytes())
     logging.info(f"UVDS binary available at: {uvds_fp}")
 
 
@@ -52,11 +53,11 @@ def main(dirOrfilePath: str, uvds_fp: str) -> None:
     uvds_fp_wext = re.sub(r"\..*", ".uvds", uvds_fp)
     if os.path.isfile(uvds_fp_wext):
         raise FileExistsError(f"provided uvds output path: {uvds_fp_wext} already exists.")
-    # a directory of .dcm files was provided
-    if os.path.isdir(dirOrfilePath):
+    if os.path.isdir(dirOrfilePath):  # a directory of .dcm files was provided
         files: List[pydicom.FileDataset] = []
         ordered_files_list = glob.glob(os.path.join(dirOrfilePath, "*.dcm"), recursive=False)
         ordered_files_list.sort()
+        logging.info(f"reading {len(ordered_files_list)} DICOM files ...")
         for fname in ordered_files_list:
             files.append(pydicom.dcmread(fname, force=False))
         width = files[0].Columns
@@ -65,46 +66,41 @@ def main(dirOrfilePath: str, uvds_fp: str) -> None:
         voxel_depth = get_voxel_depth([f.SliceLocation.real for f in files])
         texture_format = texture_format_mapping[re.sub(r"[0-9]", "", files[0].PhotometricInterpretation)]
         # data has to be converted to densities
-        data = []
+        data = np.empty(nbrslices * width * height, dtype=np.float32)
         for idx_slice in range(nbrslices):
             logging.info(f"converting slice number: {idx_slice + 1}\t of file: {files[idx_slice].filename}")
-            for idx_row in range(height):
-                for idx_col in range(width):
-                    # idx_pixel_in_slice = (idx_row * width) + idx_col
-                    # idx_pixel_in_data = (idx_slice * width * height) + idx_pixel_in_slice
-                    pixel_in_hounsfield_unit = (
-                        files[idx_slice].pixel_array[idx_row, idx_col] * files[idx_slice].RescaleSlope
-                        + files[idx_slice].RescaleIntercept
-                    )
-                    data.append(pixel_in_hounsfield_unit)
+            flattened_array = files[idx_slice].pixel_array.flatten() * np.float32(
+                files[idx_slice].RescaleSlope
+            ) + np.float32(files[idx_slice].RescaleIntercept)
+            data[idx_slice * width * height : (idx_slice + 1) * width * height] = flattened_array
         uvds_data = UVDS(
-            imagewidth=width,
-            imageheight=height,
-            nbrslices=nbrslices,
-            voxeldimX=files[0].PixelSpacing[0].real,
-            voxeldimY=files[0].PixelSpacing[1].real,
-            voxeldimZ=voxel_depth,
-            textureformat=texture_format,
+            imagewidth=np.uint16(width),
+            imageheight=np.uint16(height),
+            nbrslices=np.uint16(nbrslices),
+            voxeldimX=np.float32(files[0].PixelSpacing[0]),
+            voxeldimY=np.float32(files[0].PixelSpacing[1]),
+            voxeldimZ=np.float32(voxel_depth),
+            textureformat=np.uint8(texture_format),
             densities=data,
         )
         write_udvs_binary(uvds_data, uvds_fp_wext)
         return
-    # a path to a .dcm file was provided
-    if os.path.isfile(dirOrfilePath):
+    if os.path.isfile(dirOrfilePath):  # a path to a single .dcm file was provided
+        logging.info("single DICOM file was provided. Volumetric visualization is therefore not possible.")
         dataset: pydicom.FileDataset = pydicom.dcmread(dirOrfilePath)
-        # pixel data can be float, double float or simply bytes
-        data = dataset.pixel_array
-        # convert pixel data to HU (mater density units)
-        pass
+        texture_format = texture_format_mapping[re.sub(r"[0-9]", "", dataset.PhotometricInterpretation)]
+        data = dataset.pixel_array.flatten() * np.float32(dataset.RescaleSlope) + np.float32(
+            dataset.RescaleIntercept
+        )
         uvds_data = UVDS(
-            imagewidth=0,
-            imageheight=0,
-            nbrslices=1,
-            voxeldimX=0.0,
-            voxeldimY=0.0,
-            voxeldimZ=0,
-            textureformat=0,
-            densities=[],
+            imagewidth=np.uint16(dataset.Columns),
+            imageheight=np.uint16(dataset.Rows),
+            nbrslices=np.uint16(1),
+            voxeldimX=np.float32(dataset.PixelSpacing[0]),
+            voxeldimY=np.float32(dataset.PixelSpacing[1]),
+            voxeldimZ=np.float32(0.0),
+            textureformat=np.uint8(texture_format),
+            densities=data,
         )
         write_udvs_binary(uvds_data, uvds_fp_wext)
         return
@@ -115,5 +111,5 @@ if __name__ == "__main__":
     logging.root.setLevel(logging.INFO)
     main(
         "/home/walidchtioui/ct_datasets/example02",
-        "/home/walidchtioui/Projects/Unity-Immersive-Analytics/Datasets/test.uvds",
+        "/home/walidchtioui/Projects/Unity-Immersive-Analytics/Datasets/test_04.uvds",
     )
