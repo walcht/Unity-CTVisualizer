a24c0188b785a40211ca6e8ef38620401dfe07a5
madrienn1 <mesziadrienn@gmail.com>
2024-01-27 22:46:25 +0100

    add transfer function

--- Assets/Scripts/TransferFunctions/TransferFunction1D.cs
+++ Assets/Scripts/TransferFunctions/TransferFunction1D.cs
@@ -5,14 +5,23 @@ namespace UnityCTVisualizer
 {
     public struct ControlColor
     {
-        float position;
-        Color color;
+        public float position;
+        public Color color;
+        public ControlColor(float position, Color color) {
+            this.position = position;
+            this.color = color;
+        }
     }
 
     public struct ControlAlpha
     {
-        float position;
-        float alpha;
+        public float position;
+        public float alpha;
+
+        public ControlAlpha(float position, float alpha) {
+            this.position = position;
+            this.alpha = alpha;
+        }
     }
 
     public static class Name
@@ -45,18 +54,93 @@ namespace UnityCTVisualizer
             TextureFormat texFormat = SystemInfo.SupportsTextureFormat(TextureFormat.RGBAHalf)
                 ? TextureFormat.RGBAHalf
                 : TextureFormat.RGBAFloat;
-            texture = new Texture2D(textureWidth, textureHeight, texFormat, false);
+            Texture2D texture = new Texture2D(textureWidth, textureHeight, texFormat, false);
             Color[] pixelColorData = new Color[textureWidth * textureHeight];
             // TODO: [Adrienne] populate the pixelColorData array by interpolating provided control
             //                  colors and opacities.
-
+            List<ControlColor> sortedColorControls = new(colorControls);
+            List<ControlAlpha> sortedAlphaControls = new(alphaControls);
             // 1.   sort both control arrays according to their position
+            sortedColorControls.Sort((x, y) => x.position.CompareTo(y.position));
+            sortedAlphaControls.Sort((x, y) => x.position.CompareTo(y.position));
+            
             // 2.   add color and opacity controls at the extremities if they don't exist.
             //      make sure these extend their closes control points
+            if (sortedColorControls.Count == 0) {
+                sortedColorControls.Add(new ControlColor(1.0f, Color.white));
+            }
+            ControlColor lastColorControlPoint = sortedColorControls[sortedColorControls.Count - 1];
+            if (lastColorControlPoint.position < 1.0f) {
+                sortedColorControls.Add(new ControlColor(1.0f, lastColorControlPoint.color));
+            }
 
-            // 3.   map texture width index to the range [0.0, 1.0]. We call that value t
-            // 4.   determine left control color point index and left control opacity index
-            // 5.   initialize left and right color and alpha control points
+            ControlColor firstColorControlPoint = sortedColorControls[0];
+            if (firstColorControlPoint.position > 1.0f) {
+                sortedColorControls.Insert(0, new ControlColor(1.0f, firstColorControlPoint.color));
+            }
+
+            if (sortedAlphaControls.Count == 0) {
+                sortedAlphaControls.Add(new ControlAlpha(1.0f, 1.0f));
+            }
+
+            ControlAlpha lastAlphaControlPoint = sortedAlphaControls[sortedAlphaControls.Count - 1];
+            if (lastAlphaControlPoint.position < 1.0f) {
+                sortedAlphaControls.Add(new ControlAlpha(1.0f, lastAlphaControlPoint.alpha));
+            }
+
+            ControlAlpha firstAlphaControlPoint = sortedAlphaControls[0];
+            if (firstAlphaControlPoint.position > 1.0f) {
+                sortedAlphaControls.Insert(0, new ControlAlpha(1.0f, firstAlphaControlPoint.alpha));
+            }
+           
+            int colorControlIndex = 0;
+            int alphaControlIndex = 0;
+            int numOfColors = sortedColorControls.Count;
+            int numOfAlphas = sortedAlphaControls.Count;
+
+            // 3. map texture width index to the range [0.0, 1.0]. We call that value t
+            for (int textureIndex = 0; textureIndex < textureWidth; textureIndex++) {
+                float currentDensity = textureIndex / (float)(textureWidth - 1); // calculate density value for current index
+                // find nearest left color control point to density
+                while (colorControlIndex < numOfColors - 2 && sortedColorControls[colorControlIndex + 1].position < currentDensity) {
+                    colorControlIndex++;
+                }
+                // find nearest left alpha control point to density
+                while (alphaControlIndex < numOfColors - 2 && sortedAlphaControls[alphaControlIndex + 1].position < currentDensity) {
+                    alphaControlIndex++;
+                }
+
+                ControlColor leftColorControl = sortedColorControls[colorControlIndex];
+                ControlColor rightColorControl = sortedColorControls[colorControlIndex + 1];
+                ControlAlpha leftAlphaControl = sortedAlphaControls[alphaControlIndex];
+                ControlAlpha rightAlphaControl = sortedAlphaControls[alphaControlIndex + 1];
+                // Min-Max scaling normalization for density, map density to range [0.0, 1.0]
+                // 1. left color control point position - 0.0, right color control position - 1.0
+                // 2. left alpha control point position - 0.0, right alpha control position - 1.0
+                float normalizedDensityForColor = (Mathf.Clamp(currentDensity, leftColorControl.position, rightColorControl.position) - leftColorControl.position) /
+                    (rightColorControl.position - leftColorControl.position);
+                float normalizedDensityForAlpha = (Mathf.Clamp(currentDensity, leftAlphaControl.position, rightAlphaControl.position) - leftAlphaControl.position) /  
+                    (rightAlphaControl.position - leftAlphaControl.position);
+
+                // Interpolates between min and max with smoothing at the limits. smooth(min, max, t)
+                // similar to Lerp, but the interpolation will gradually speed up from the start and slow down toward the end
+                float smoothedDensityForColor = Mathf.SmoothStep(0.0f, 1.0f, normalizedDensityForColor);
+                float smoothedAlphaForColor = Mathf.SmoothStep(0.0f, 1.0f, normalizedDensityForAlpha);
+
+                // linear interpolation
+                Color pixelColor = rightColorControl.color * smoothedDensityForColor + leftColorControl.color * (1.0f - smoothedDensityForColor);
+                pixelColor.a = rightAlphaControl.alpha * smoothedAlphaForColor + leftAlphaControl.alpha * (1.0f - smoothedAlphaForColor);
+
+                pixelColorData[textureIndex] = QualitySettings.activeColorSpace == ColorSpace.Linear ? pixelColor.linear : pixelColor;   
+                
+                texture.wrapMode = TextureWrapMode.Clamp;
+                texture.SetPixels(pixelColorData);
+                texture.Apply();
+            }
+
+
+            // 4.   determine left control color point index and left control opacity index - done
+            // 5.   initialize left and right color and alpha control points - done
             // 6.   map current t value to range [0.0, 1.0] where 0.0 means t = leftColor.position
             //      and 1.0 means t = rightColor.position
             // 7.   do the same as previous step for opacity
@@ -65,7 +149,7 @@ namespace UnityCTVisualizer
             // 10.  populate pixelColorData array at current index and don't forget to take the color
             //      space into account!
 
-            texture.SetPixel(pixelColorData);
+            texture.SetPixels(pixelColorData);
             return texture;
         }
     }
